464d8da607889a797d372dc6c4bc9af6df3d509c
Krste Kovaceski <krste@pragdigi.com>
2019-09-09 14:10:00 +0200

    add styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "./404.module.css";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className={styles.header}>NOT FOUND</h1>
+    <p className={styles.error-message}>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
